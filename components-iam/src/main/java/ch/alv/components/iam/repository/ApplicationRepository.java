500b7fa7c1b15c8411ab3080973470d38dbca423
stibos <stibeheller@gmail.com>
2014-03-27 21:08:58 +0100

    clean up

--- components-iam/src/main/java/ch/alv/components/iam/repository/ApplicationRepository.java
+++ components-iam/src/main/java/ch/alv/components/iam/repository/ApplicationRepository.java
@@ -1,14 +1,14 @@
 package ch.alv.components.iam.repository;
 
-import ch.admin.seco.tcsb.modules.iam.model.Application;
+import ch.alv.components.iam.model.Application;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository interface for {@link Application} entities
  *
- * @author seco-hrf
  * @since 1.0.0
  */
 @Repository
-public interface ApplicationRepository extends PagingAndSortingRepository<Application, String>, ApplicationRepositoryCustom {}
+public interface ApplicationRepository extends PagingAndSortingRepository<Application, String>, ApplicationRepositoryCustom {
+}
