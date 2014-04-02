56672fbb7fa83299d16e631296280a5d1ff93979
stibos <stibeheller@gmail.com>
2014-04-02 07:09:44 +0200

    Throw a "bad request" if a TextConstant does not exist (Language not supported).

--- components-webapi/src/main/java/ch/alv/components/web/exception/GlobalExceptionController.java
+++ components-webapi/src/main/java/ch/alv/components/web/exception/GlobalExceptionController.java
@@ -1,5 +1,6 @@
 package ch.alv.components.web.exception;
 
+import ch.alv.components.persistence.i18n.NoSuchTextConstantException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,11 @@ public class GlobalExceptionController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleAllExceptions(Exception e) {
+
+        if (e instanceof NoSuchTextConstantException) {
+            return new ResponseEntity<>("The requested language is not or at least not fully supported...", HttpStatus.BAD_REQUEST);
+        }
+
         UUID uuid = UUID.randomUUID();
         LOG.error("Exception id: " + uuid.toString(), e);
         return new ResponseEntity<>("A internal server error occurred. Please inform our support staff with the error id: " + uuid.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
