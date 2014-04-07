29a5b4bfe3395210acbe5103eb7019ae5553e66a
stibos <stibeheller@gmail.com>
2014-04-07 08:19:28 +0200

    Removed obsolete stuff.

--- components-persistence/src/main/java/ch/alv/components/persistence/query/internal/QueryPredicateItem.java
+++
@@ -1,9 +0,0 @@
-package ch.alv.components.persistence.query.internal;
-
-/**
- * Marker interface for {@link QueryPredicate} and {@link QueryPredicateGroup} objects.
- *
- * @since 1.0.0
- */
-public interface QueryPredicateItem {
-}
