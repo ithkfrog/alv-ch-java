2ca4f0de9060e84df5fbb43d3b53bbcda73aab12
stibos <stibeheller@gmail.com>
2014-04-12 20:58:15 +0200

    Centralize constants.

---
+++ components-webapi/src/main/java/ch/alv/components/web/WebConstant.java
@@ -0,0 +1,14 @@
+package ch.alv.components.web;
+
+/**
+ * Constants used in the components-web module
+ *
+ * @since 1.0.0
+ */
+public interface WebConstant {
+
+    String PARAM_NAME_PAGE = "page";
+    String PARAM_NAME_PAGE_SIZE = "pageSize";
+    String PARAM_NAME_SEARCH = "searchName";
+
+}
