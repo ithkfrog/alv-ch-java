0f9d97ea79741ecf63f7abb7219f64a2d19c0bc9
stibos <stibeheller@gmail.com>
2014-04-26 13:30:56 +0200

    Remove unused constructors.

--- components-core/src/main/java/ch/alv/components/core/beans/mapper/MappingException.java
+++ components-core/src/main/java/ch/alv/components/core/beans/mapper/MappingException.java
@@ -7,18 +7,8 @@ package ch.alv.components.core.beans.mapper;
  */
 public class MappingException extends RuntimeException {
 
-    public MappingException() {
-    }
-
-    public MappingException(String message) {
-        super(message);
-    }
-
     public MappingException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public MappingException(Throwable cause) {
-        super(cause);
-    }
 }
