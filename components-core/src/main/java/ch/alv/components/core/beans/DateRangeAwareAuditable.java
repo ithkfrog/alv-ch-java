66addc35065a638ceed1427f408fdbbfdc3bf040
stibos <stibeheller@gmail.com>
2014-08-05 07:19:20 +0200

    Enable auditing of entities.

---
+++ components-core/src/main/java/ch/alv/components/core/beans/DateRangeAwareAuditable.java
@@ -0,0 +1,9 @@
+package ch.alv.components.core.beans;
+
+/**
+ * This interface combines the {@link DateRangeAware} and the {@link Auditable} interface.
+ *
+ * @since 1.0.0
+ */
+public interface DateRangeAwareAuditable extends DateRangeAware, Auditable {
+}
